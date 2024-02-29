d8d4ec2f406b520d667ddc7a941d4daf5e1fd475
Evan Mucciolo <evan@hopperblue.com>
2024-02-28 19:33:17 -0600

    Evan addition 2.28.24

--- app.js
+++ app.js
@@ -2,4 +2,32 @@
 
 let dictList = jsonData[0]
 
-console.log(dictList)
+//console.log(jsonData)
+
+let countries = []
+
+for (let i = 0; i < jsonData.length; i++) {
+    countries.push(jsonData[i].CountryName)
+    //console.log(jsonData[i].CountryName)
+}
+
+let uniqueCountries = new Set(countries)
+
+let selector = document.querySelector("#selDataset");
+//selector.html("<option>Evan</option>")
+
+for (let i = 0; i < uniqueCountries.length; i++){
+    selector.append(`<option> ${uniqueCountries[i]} </option>`)
+    //.property("value",uniqueCountries[i]);
+  };
+
+console.log(uniqueCountries)
+
+
+function charts() {
+
+    let data = jsonData[0]
+    let year = data.year
+    let percentTourismGDP = data.total
+}
+
